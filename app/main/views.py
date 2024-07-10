03593f02c786a251968f040211aa2884fbbf2c4e
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-07-10 10:24:37 +0300

    finish main app

--- app/main/views.py
+++ app/main/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Home page for store'
+        'title': 'JCY - Home',
+        'content': 'Furniture store "JCY"'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'JCY - About us',
+        'content': 'About us',
+        'text_on_page': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    }
+
+    return render(request, 'main/about.html', context)
